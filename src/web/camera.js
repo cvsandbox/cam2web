e389eff125b8ec4bd3450ce7cd10fe38394f1bca
Andrew Kirillov <andrew.kirillov@gmail.com>
2017-05-12 13:50:58 +0100

    Added very simple first web UI.

---
+++ src/web/camera.js
@@ -0,0 +1,65 @@
+var Camera = (function ()
+{
+    var jpegUrl       = '/jpeg';
+    var mjpegUrl      = '/mjpeg';
+    var mjpegMode;
+    var frameInterval;
+    var imageElement;
+    var timeStart;
+
+    function refreshImage( )
+    {
+        if ( mjpegMode )
+        {
+            imageElement.src = mjpegUrl;
+        }
+        else
+        {
+            timeStart = new Date( ).getTime( );
+            imageElement.src = jpegUrl + '?t=' + timeStart;
+        }
+    }
+    
+    function onImageError( )
+    {
+        // try rotating between MJPEG/JPEG modes - browsers like IE don't get MJPEG at all
+        mjpegMode = !mjpegMode;
+        // also give it a small pause on error
+        setTimeout( refreshImage, 1000 );
+    }
+
+    function onImageLoaded( )
+    {
+        if ( !mjpegMode )
+        {
+            var timeTaken = new Date( ).getTime( ) - timeStart;
+            setTimeout( refreshImage, ( timeTaken > frameInterval ) ? 0 : frameInterval - timeTaken );
+        }
+    }
+    
+    var start = function( fps )
+    {
+        imageElement = document.getElementById( 'camera' );
+        
+        imageElement.onload  = onImageLoaded;
+        imageElement.onerror = onImageError;
+        
+        if ( ( typeof fps == 'number' ) &&  ( fps != 0 ) )
+        {
+            frameInterval = 1000 / fps;
+        }
+        else
+        {
+            frameInterval = 100;
+        }
+        
+        // always try capturing in MJPEG mode
+        mjpegMode = true;
+        
+        refreshImage( );
+    };
+
+    return {
+        Start: start
+    }
+} )( );
